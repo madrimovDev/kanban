157707792054c1cb04cd5a64d50c53a4e44b2799
madrimovDev <madrimov5014@gmail.com>
2023-04-17 18:35:55 +0500

    home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,16 @@
 import React from 'react'
 
 export default function Home() {
-	return <div>Home</div>
+	return (
+		<div className='w-full h-full grid place-items-center'>
+			<div>
+				<h1 className='text-4xl'>React Kanban</h1>
+				<ol className='list-decimal list-inside space-y-3 mt-3'>
+					<li className='text-xl'>Create Board</li>
+					<li className='text-xl'>Create Column</li>
+					<li className='text-xl'>Create Task</li>
+				</ol>
+			</div>
+		</div>
+	)
 }
